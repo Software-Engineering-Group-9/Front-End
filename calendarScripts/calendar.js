a4853da6e465d2c9f93bbaadd71bc3360e2dca01
nehpethP <sarkis8@uwindsor.ca>
2020-11-27 22:57:23 -0500

    updated fetch calls. no longer add todo events to the calendar.

--- calendarScripts/calendar.js
+++ calendarScripts/calendar.js
@@ -15,7 +15,42 @@ var cal = new tui.Calendar('#calendar', {
   disableDblClick: true,
 });
 
-//FETCH REQUEST TO FILL CALENDAR HERE
+//fetch request to fill calendar upon login
+fetch("http://localhost:8080/api/v1/calendar/createAvailability", {
+    method: 'GET',
+    headers: {
+      'authorization': getCookie('access_token')
+    }
+  })
+  .then(function(response) {
+    if (!response.ok) {
+      response.json().then(function(object) {
+        console.error('Error:', error);
+      });
+    }
+    return response.json();
+  })
+  .then(function(data) {
+
+    for (var key in j) {
+      console.log(j[key]);
+      var newSchedule = {
+        id: j[key].id,
+        calendarid: '1',
+        title: j[key].title,
+        category: 'time',
+        start: j[key].starttime,
+        end: j[key].endtime,
+        bgColor: j[key].color,
+        dragBgColor: j[key].color
+      }
+      cal.createSchedules(newSchedule);
+    }
+
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 
 cal.on('beforeCreateSchedule', function(event) {
 
@@ -33,11 +68,11 @@ cal.on('beforeCreateSchedule', function(event) {
     bgColor: busyColour,
     dragBgColor: busyColour,
   };
-  cal.createSchedules([newBusyEvent]);
 
-  fetch to send new schedules to backend
+  cal.createSchedules([newBusyEvent]);
+  //fetch to send new schedules to backend and create upon ok response
   fetch("http://localhost:8080/api/v1/calendar/createAvailability", {
-      method: 'POST',
+      method: 'GET',
       body: JSON.stringify(newBusyEvent),
       headers: {
         'Origin': ' *',
@@ -82,22 +117,12 @@ cal.on('clickSchedule', function(event) {
 
   //function to create new event with the new name and delete the old one
   document.getElementById("editChangeTitle").onclick = function() {
-    event.schedule.title = document.getElementById("busyEditTitle").value;
+    document.getElementById("busyEditTitle").value;
 
-    //create new event with new name
-    var newEvent = {
-      id: event.id,
-      calendarID: '1',
-      title: document.getElementById("busyEditTitle").value,
-      category: 'time',
-      start: formatTime(event.schedule.start._date),
-      end: formatTime(event.schedule.end._date),
-      bgColor: event.schedule.bgColor,
-      dragBgColor: event.schedule.dragBgColor,
-    };
-    cal.createSchedules([newEvent]);
-    //delete old one
-    cal.deleteSchedule(event.schedule.id, event.schedule.calendarId);
+    //update busy schedule with new name
+    cal.updateSchedule(event.schedule.id, event.schedule.calendarId, {
+      title: document.getElementById("busyEditTitle").value
+    });
 
     //change view of page after editing title
     document.getElementById("busyEditModal").style.display = "none";
@@ -117,86 +142,7 @@ cal.on('clickSchedule', function(event) {
     document.getElementById("logoutButton").style.display = "block";
   }
 
-  if (event.schedule.bgColor == '#4aadff') {
-    //change view of page
-    document.getElementById("editToDoItemModal").style.display = "block";
-    document.getElementById("logoutButton").style.display = "none";
-
-    document.getElementById("submitEditEvent").onclick = function() {
-
-      console.log("submitted");
-
-      //new fields
-      var title = document.getElementById("editTitle").value;
-      var dueDate = document.getElementById("editDueDate").value;
-      var dueTime = document.getElementById("editDueTime").value;
-      var timeNeeded = document.getElementById("editTimeNeeded").value;
-
-      //required input
-      if (title == "" || dueDate === "" || dueTime === "") {
-        document.getElementById("requiredFieldTextEdit").style.display = "block"
-        document.getElementById("requiredFieldTextEdit").innerHTML = "* indicates required input"
-        return console.log("Error: Required InputField");
-      }
-
-      //calculate start time, format properly for calendar library
-      var dueTimeArr = dueTime.split(':');
-
-      var startTimeH = parseInt(dueTimeArr[0]);
-      var startTimeM = parseInt(dueTimeArr[1]);
-
-      var startTime;
-      if (startTimeH == 0 && startTimeM < 30) {
-        startTimeH = 0;
-        startTimeM = 0;
-      } else if (startTimeM < 30 && startTimeH != 0) {
-        startTimeM += 30;
-        startTimeH--;
-      } else {
-        startTimeM -= 30;
-      }
-
-      if (startTimeM < 10 && startTimeH < 10) {
-        var startTime = '0' + startTimeH + ':0' + startTimeM;
-      } else if (startTimeH < 10) {
-        var startTime = '0' + startTimeH + ':' + startTimeM;
-      } else if (startTimeM < 10) {
-        var startTime = startTimeH + ':0' + startTimeM;
-      } else if (startTimeH == 0 && startTimeM < 30) {
-        startTime = '00:00';
-      } else {
-        var startTime = startTimeH + ':' + startTimeM;
-      }
-
-      //edit time format for calendar library
-      var noon = "am"
-      if (dueTimeArr[0] > 12) {
-        dueTimeArr[0] -= 12;
-        noon = "pm";
-      }
-
-      //variables and formatting of event list items
-      var TitleString = title;
-      if (timeNeeded != "") {
-        TitleString += " - (" + timeNeeded + "h)"
-      }
-      var resString = dueDate + "   " + dueTimeArr[0] + ":" + dueTimeArr[1] + noon;
-
-      //update toDoItem text
-      document.getElementById(event.schedule.id).innerHTML = "<b>" + TitleString + "</b><br> Due: " + resString;
-
-      //create fetch here?
-
-      closeEditEventForm();
-
-      cal.updateSchedule(event.schedule.id, event.schedule.calendarId, {
-        title: title,
-        start: dueDate + 'T' + startTime + ':00',
-        end: dueDate + 'T' + dueTime + ':00',
-      });
-    }
-
-  } else if (event.schedule.bgColor == busyColour) {
+  if (event.schedule.bgColor == busyColour) {
     //change view of page when editing
     document.getElementById("busyEditModal").style.display = "block";
     document.getElementById("logoutButton").style.display = "none";
@@ -277,60 +223,43 @@ function getMonthNum(month) {
   return monthNum;
 }
 
-//adds new event into the calendar
-function addEvent(newEvent) {
+function calendarOptimize(){
+  console.log("TIME TO OPTIMIZE");
 
-  //create the schedule for the calendar
-  cal.createSchedules([{
-    id: newEvent.id,
-    calendarID: '1',
-    title: newEvent.title,
-    category: 'time',
-    dueDateClass: '',
-    start: newEvent.dueDate + 'T' + newEvent.startTime + ':00',
-    end: newEvent.dueDate + 'T' + newEvent.dueTime + ':00',
-    bgColor: '#4aadff',
-    dragBgColor: '#4aadff',
-  }]);
-
-  //reset the fields of the createEvent form to blank
-  document.getElementById("title").value = "";
-  document.getElementById("dueDate").value = "";
-  document.getElementById("dueTime").value = "";
-  document.getElementById("timeNeeded").value = "";
-
-  //fetch to send the newly created event info to the backend
-  fetch("http://localhost:8080/api/v1/calendar/create", {
-      method: 'POST',
-      body: JSON.stringify(newEvent),
+  fetch("http://localhost:8080/api/v1/calendar/createAvailability", {
+      method: 'GET',
       headers: {
-        'Origin': ' *',
-        'Accept': 'application/json',
         'authorization': getCookie('access_token')
       }
     })
     .then(function(response) {
       if (!response.ok) {
         response.json().then(function(object) {
-          document.getElementById("loginInvalidEmail").innerHTML = object.message;
+          console.error('Error:', error);
         });
       }
-      // else {
-      //   cal.createSchedules([{
-      //     id: seconds,
-      //     calendarID: '1',
-      //     title: newEvent.title,
-      //     category: 'time',
-      //     dueDateClass: '',
-      //     start: newEvent.dueDate + 'T' + newEvent.startTime + ':00',
-      //     end: newEvent.dueDate + 'T' + newEvent.dueTime + ':00',
-      //     bgColor: '#4aadff',
-      //     dragBgColor: '#4aadff',
-      //   }]);
-      // }
+      return response.json();
+    })
+    .then(function(data) {
+
+      for (var key in j) {
+        console.log(j[key]);
+        var newSchedule = {
+          id: j[key].id,
+          calendarID: '1',
+          title: j[key].title,
+          category: 'time',
+          start: j[key].starttime,
+          end: j[key].endtime,
+          bgColor: j[key].color,
+          dragBgColor: j[key].color
+        }
+        var cid = ""
+        cal.deleteSchedule(id,cid);
+        cal.createSchedules(newSchedule);
+      }
     })
     .catch((error) => {
       console.error('Error:', error);
     });
-
 }
